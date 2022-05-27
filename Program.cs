5a1faedfcbb28105a201863f7371351aa2b0ff2c
Michael <91434795+usernameisavailablee@users.noreply.github.com>
2022-05-28 00:49:51 +0300

    check

--- Program.cs
+++ Program.cs
@@ -3,22 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
 
 namespace Kek
 {
     static class Program
     {
-        //Z
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+            Document doc = app.Documents.Add(Visible: true);
+            doc.Save();
+            doc.Close();
+            app.Quit();
+            Console.ReadKey();
+            System.Windows.Forms.Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new Form1());
         }
     }
 }
