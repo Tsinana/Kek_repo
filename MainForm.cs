69e2390f213c4729a648b97474eef475f173da83
Tsinana <hlebusru@gmail.com>
2022-05-30 02:18:05 +0300

    Земля

--- MainForm.cs
+++ MainForm.cs
@@ -193,28 +193,22 @@ namespace Kek
                 doc.Close();
                 _app.Quit();
 
+                Excel.Workbook workBook = ex.Workbooks.Add();
+                Excel.Worksheet sheet = (Excel.Worksheet)ex.Worksheets.get_Item(1);
+
+                int ii = 0;
+                int jj = 0;
                 foreach (Variant version in listVar)
                 {
+                    jj = 0;
                     string name = version.GetName();
                     List<string> listStr = version.GetAnsValue();
                     foreach (string str in listStr)
                     {
-
+                        sheet.Cells[ii, jj] = String.Format(str);
+                        jj++;
                     }
-                }
-
-
-                    Excel.Workbook workBook = ex.Workbooks.Add();
-                Excel.Worksheet sheet = (Excel.Worksheet)ex.Worksheets.get_Item(1);
-
-
-
-
-                for (int i = 1; i <= 9; i++)
-                {
-                    
-                    for (int j = 1; j < 9; j++)
-                        sheet.Cells[i, j] = String.Format("Boom {0} {1}", i, j);
+                    ii++;
                 }
 
                 workBook.Save();
