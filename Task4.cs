4d483469e6d4c6d62bd093eaf21ae640ca3e0049
Александр Николаевич Мищенко <alex1mishchenko@gmail.com>
2022-05-30 12:33:13 +0300

    Все, кроме матрицы

--- Task4.cs
+++ Task4.cs
@@ -5,11 +5,13 @@ namespace Kek
     class Task4
     {
         private string template { get; set; }
+        private double x;
+        private double npq;
+        private double xLap1;
+        private double xLap2;
+        private double lev;
+        private double prav;
 
-        public string GetAnsValue()
-        {
-            return "";//Convert.ToString(xLap1);
-        }
 
         public string GetValue()
         {
@@ -22,13 +24,25 @@ namespace Kek
             int n = rand.Next(150, 201);
             int k = rand.Next(2, 6);
             int kk = 150 + rand.Next(15, 36);
-            double p = Math.Round(rand.NextDouble(),2);
+            double p;
+            do
+                 p = Math.Round(rand.NextDouble(), 2);
+            while (p == 0.0);
             double q = 1 - p;
             double x = (k - (n * p)) / (Math.Sqrt(n * p * q));
+            this.x = x;
             int k1 = kk + 1;
             int k2 = n;
+            double npq = Math.Sqrt(n * p * q);
+            this.npq = npq;
             double xLap1 = Math.Round((k1 - (n * p)) / (Math.Sqrt(n * p * q)),2);
             double xLap2 = Math.Round((k2 - (n * p)) / (Math.Sqrt(n * p * q)), 2);
+            this.xLap1 = xLap1;
+            this.xLap2 = xLap2;
+            double lev = n * p - q;
+            double prav = n * p + p;
+            this.lev = lev;
+            this.prav = prav;
             string template;
             bool tackType = Convert.ToBoolean(rand.Next(0, 2));
             if (tackType)
@@ -37,6 +51,13 @@ namespace Kek
                 template = $"На станциях отправления поездов находится {n} автоматов для продажи билетов. Вероятность выхода из строя одного автомата в течение часа равна {p}. Найти вероятность того, что в течение часа выйдут из строя: а) {k} автоматов(a); б) от {k1} до {k2} автоматов. Найти наиболее вероятное число вышедших из строя автоматов.";
             this.template = template;
             //Значение функций считать по таблице ептаааа//
+        }
+        public string GetAnsValue()
+        {
+            char fis = Convert.ToChar(966);
+            char Fi = Convert.ToChar(934);
+            char men = Convert.ToChar(60);
+            return Convert.ToString($"a) P(k) = {fis}({x})/{npq}   б){Fi}({xLap2})={Fi}({xLap1}) в){lev}{men}=k{men}{prav}");
         }
     }
 }
